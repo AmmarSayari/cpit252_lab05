c0783394e110304488ef06a170a3349234a6e519
Khalid Alharbi <kalharbi@users.noreply.github.com>
2022-02-27 10:58:47 +0300

    Initial commit

---
+++ src/main/java/edu/kau/fcit/cpit252/App.java
@@ -0,0 +1,10 @@
+package edu.kau.fcit.cpit252;
+
+import edu.kau.fcit.cpit252.geoLocation.GeoLocation;
+import edu.kau.fcit.cpit252.weatherDB.WeatherDBI;
+
+public class App {
+    public static void main(String[] args) {
+        // Jeddah => 21.543333, 39.172778
+    }
+}
---
+++ src/main/java/edu/kau/fcit/cpit252/weatherDB/WeatherDBI.java
@@ -0,0 +1,60 @@
+package edu.kau.fcit.cpit252.weatherDB;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.kau.fcit.cpit252.WeatherCity;
+import edu.kau.fcit.cpit252.utils.HTTPHelper;
+import org.apache.hc.core5.net.URIBuilder;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+
+// The Adaptee class, WeatherDBI, takes a city name and returns the weather forecast.
+public class WeatherDBI implements WeatherCity {
+
+    private final String API_URL = "https://weatherdbi.herokuapp.com/data/weather/";
+
+    @Override
+    public String getWeatherInfo(String city) {
+        // build the URL
+        WeatherInfo wInfo = null;
+        URIBuilder b = null;
+        try {
+            b = new URIBuilder(API_URL + city);
+            URI uri = b.build();
+            HttpResponse<String> response = HTTPHelper.sendGet(uri);
+            System.out.println(response.body());
+            if (response != null) {
+                return "23c";
+//                wInfo = parseWeatherResponse(response.toString(), WeatherInfo.class);
+//                return wInfo.toString();
+            }
+
+            return "failed";
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+            return e.getMessage();
+        }
+
+    }
+
+
+    public static WeatherInfo parseWeatherResponse(String responseString, Class<?> elementClass){
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            JsonNode weatherInfoNode = objectMapper.readTree(responseString);
+            WeatherInfo wInfo = new WeatherInfo();
+            System.out.println(responseString);
+            wInfo.setTime("weatherInfoNode.get(\"name\").textValue()");
+            wInfo.setTime("weatherInfoNode.get(\"name\").get(\"c\").textValue()");
+            wInfo.setTime("weatherInfoNode.get(\"name\").get(\"c\").textValue()");
+
+            return wInfo;
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
